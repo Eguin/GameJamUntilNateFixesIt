3920da3f4e1491d4bb5f3b942e06859285b3a973
Eguin <eliyahuavir@gmail.com>
2020-12-28 15:44:20 -0500

    12/28 3:43

---
+++ Assets/Scripts/Game Scripts/CollectableObjects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableObjects : MonoBehaviour
+{
+    GameObject player;
+
+    float distanceFromObject;
+
+    Collectables collectableScript;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position)<distanceFromObject)
+        {
+            collected();
+        }
+    }
+
+    public void setPlayer(GameObject p)
+    {
+        player = p;
+    }
+    
+    public void setDistance(float d)
+    {
+        distanceFromObject = d;
+    }
+
+    public void setCollectablesScript(Collectables col)
+    {
+        collectableScript = col;
+    }
+
+    void collected()
+    {
+        collectableScript.removeItem(this);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Game Scripts/Collectables.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectables : MonoBehaviour
+{
+    [SerializeField]
+    List<CollectableObjects> objectsToCollect;
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float distanceFromPlayer;
+
+    [SerializeField]
+    Text itemsList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemsList.text = "";
+        for(int i = 0; i < objectsToCollect.Count ; i++)
+        {
+            itemsList.text += objectsToCollect[i].gameObject.ToString() + "\n";
+            objectsToCollect[i].setPlayer(player);
+            objectsToCollect[i].setDistance(distanceFromPlayer);
+            objectsToCollect[i].setCollectablesScript(this);
+        }
+    }
+
+    public void removeItem(CollectableObjects collectedObject)
+    {
+        objectsToCollect.Remove(collectedObject);
+        for (int i = 0; i < objectsToCollect.Count; i++)
+        {
+            itemsList.text += objectsToCollect[i].gameObject.ToString() + "\n";
+        }
+        if (objectsToCollect.Count == 0)
+        {
+            //end level
+        }
+    }
+}
